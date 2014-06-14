003430d5b823cda5ae53cb76b4e75ac5815786f2
rg3915 <rg3915@yahoo.com.br>
2014-06-13 22:36:22 -0300

    calculadora com Python3

--- pythonBasico/calculadora/usando_calculadora.py
+++ pythonBasico/calculadora/usando_calculadora.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 from calculadora import Calculadora
 
-c = Calculadora(128,2)
+c = Calculadora(128, 2)
 
-print 'Soma:', c.soma()
-print 'Subtração:', c.subtrai()
-print 'Multiplicação:', c.multiplica()
-print 'Divisão:', c.divide()
+print('Soma:', c.soma())
+print('Subtração:', c.subtrai())
+print('Multiplicação:', c.multiplica())
+print('Divisão:', c.divide())
 
 c.a = 12
 c.b = 42
 
-print c.soma()
+print(c.soma())
